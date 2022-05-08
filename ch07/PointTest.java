6ac691f8eac420c4cecafc990a03d605de84e352
hyunha95 <nohyunha95@gmail.com>
2022-05-08 20:55:37 +0900

    복습

---
+++ ch07/PointTest.java
@@ -0,0 +1,28 @@
+package ch07;
+
+public class PointTest {
+}
+
+//class Point {
+//    int x, y;
+//
+//    Point(int x, int y) {
+//        this.x = x;
+//        this.y = y;
+//    }
+//    String getLocation() {
+//        return "x : " + x + ", y :" + y;
+//    }
+//}
+
+//class Point3D extends Point {
+//    int z;
+//
+//    Point3D(int x, int y, int z) {
+//        super(x, y);
+//        this.z = z;
+//    }
+//    String getLocation() {
+//        return super.getLocation() + ", z :" + z;
+//    }
+//}
