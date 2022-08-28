93489341d84f26ff880d295a727bfd76c18da626
nohyunha95 <nohyunha95@gmail.com>
2022-08-28 23:18:15 +0900

    소켓

--- ch16/TcpIpServer.java
+++ ch16/TcpIpServer.java
@@ -1,6 +1,9 @@
 package ch16;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.net.ServerSocket;
@@ -19,7 +22,23 @@ public class TcpIpServer {
 
 		while(true) {
 			try {
+				System.out.println(getTime() + "연결요청을 기다립니다.");
+				// 서버소켓은 클라리언트의 연결요청이 올 떄까지 실행을 멈추고 계속 기다린다.
+				// 클라이언트의 연결요청이 오면 클라이언트 소켓과 통신할 새로운 소켓을 생성한다.
+				Socket socket = serverSocket.accept();
+				System.out.println(getTime() + socket.getInetAddress() + "로부터 연결요청이 들어왔습니다.");
 
+				// 소켓의 출력스트림을 얻는다.
+				OutputStream out = socket.getOutputStream();
+				DataOutputStream dos = new DataOutputStream(out);
+
+				// 원격 소켓(remote socket)에 데이터를 보낸다.
+				dos.writeUTF("[Notice] Test Message1 from Server");
+				System.out.println(getTime() + "데이터를 전송했습니다.");
+
+				// 스트림과 소켓을 닫아준다.
+				dos.close();
+				socket.close();
 			} catch(IOException e) {
 				e.printStackTrace();
 			}
